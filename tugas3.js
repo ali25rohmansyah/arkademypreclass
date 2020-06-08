369ee785c43b788b631d75424992ae9f6b8f77ad
@ali25rohmansyah <ali25rohmansyah@gmail.com>
2020-06-08 18:52:41 +0700

    add tugas 2 dan 3

---
+++ tugas3.js
@@ -0,0 +1,69 @@
+// Ini comment satu baris
+/* Tipa data di JavaScript adalah
+1. Number
+2. String
+3. Boolean
+4. Array
+5. Object
+*/
+
+// Tipe data Number 
+let umur = 22
+let berat = 60
+let tinggi = 169
+
+// Tipe data String
+let hobi = "Membaca"
+const namaPertama = "Ali"
+const namaKedua = "Rohmansyah"
+
+// Tipe data boolean
+const sayaGanteng = true
+let sayaMembawaHp = true
+let sayaMemakaiKemeja = false
+
+// Tipe data array
+let hewan = ["kambing, sapi, ayam"]
+let bendaMati = ["botol", "kacamata", "kursi"]
+let makanan = ["mie ayam", "pasta", "bubur ayam"]
+
+// Tipe data objek
+let biodata = {
+    nama: "Ali Rohmansyah",
+    pekerjaan: "Mobile Developer",
+    umur: 22
+}
+let alamat = {
+    negara: "indonesia",
+    kota: "bekasi",
+    provinsi: "jawa barat",
+    kec: "jati asih",
+    kel: "jatikramat"
+}
+
+let car = {
+    tipe:"Fiat", 
+    model:"500", 
+    warna:"white"
+}
+
+const angkaPertama = 2020
+const angkaKedua = 8
+let total
+console.log(`angkaPertama = ${angkaPertama}, angkaKedua = ${angkaKedua}`)
+
+// Penjumlahan 
+total = angkaPertama + angkaKedua
+console.log(total)
+
+// Pengurangan
+total = angkaPertama - angkaKedua
+console.log(total)
+
+// Pembagian
+total = angkaPertama / angkaKedua
+console.log(total)
+
+// Perkalian
+total = angkaPertama * angkaKedua
+console.log(total)
