eb7ac3455bd27f82e3533c576adf44656ad9b5d3
@ali25rohmansyah <ali25rohmansyah@gmail.com>
2020-06-08 17:55:20 +0700

    add tugas 1

---
+++ tugas1.js
@@ -0,0 +1,13 @@
+const nama = "Ali Rohmansyah"
+const umur = 22
+const alamat = "Jl. Ta'lim No. 62 Rt008/Rw004, Jatikramat, Jatiasih, Bekasi, Jawa Barat, 17421"
+const hobi = "Membaca novel/komik"
+const asalSekolah = "Universitas Gunadarma, jurusan Teknik Informatika"
+const tertarikProgramming = "Karena saya selalu memikirkan bagaimana merancang/membuat program"
+const alasan1 = "UI/UX, Html, Css, Javascript, kotlin/java, Php, Git, dan beberapa framework web"
+const alasan2 = "ya karena agak sulit untuk belajar tanpa mentor yg berpengalaman"
+const alasan3 = "Karena saya tertarik dengan penyaluran kerja dengan minimum gaji 6jt"
+const alasan4 = "Saya memiliki beberapa pengetahuan dasar hingga menengah terkait beberapa materi yang nanti akan diberikan"
+
+console.log(`${nama}\n${umur}\n${alamat}\n${hobi}\n${asalSekolah}\n${tertarikProgramming}\n${alasan1}\n${alasan2}\n${alasan3}\n${alasan4}`)
+
